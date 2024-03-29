16202be719c876ecd784a9784fd61daede46c123
Niall Bennett <niallbennett@Nialls-MacBook-Pro.local>
2024-03-29 21:01:00 +0000

    testing setup

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-// You wan tot test pure functions, that is funtions that do a single thing and exist outside a React component where posible.
-// import { render } from "@testing-library/react";
-// import App from "./App";
-// import { expect, it, test } from "vitest";
-
-// test("test that app fetches coin data", () => {
-//   render(<App />);
-// });
