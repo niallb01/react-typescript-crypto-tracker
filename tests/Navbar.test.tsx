b8a35d559233c5afd6999c32a67b243cf9b2728e
Niall Bennett <niallbennett@Nialls-MacBook-Pro.local>
2024-04-12 14:38:41 +0100

    navbar test

--- tests/Navbar.test.tsx
+++ tests/Navbar.test.tsx
@@ -1,18 +1,19 @@
 import Navbar from "../src/components/Navbar";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { it, expect, describe } from "vitest";
 import "@testing-library/jest-dom/vitest";
 import React from "react";
 import { MemoryRouter } from "react-router";
 
 describe("navbar", () => {
-  it("should link to home page", () => {
+  it("should show header and link to home page", () => {
     const { getByText } = render(
       <MemoryRouter>
         <Navbar />
       </MemoryRouter>
     );
-    const heading = getByText(/live coin tracker/i);
-    expect(heading).toBeInTheDocument();
+    const headerLink = getByText(/live coin tracker/i); // query method
+    expect(headerLink).toBeInTheDocument(); // test rendering
+    expect(headerLink.getAttribute("href")).toBe("/"); // getAttribute - matcher
   });
 });
