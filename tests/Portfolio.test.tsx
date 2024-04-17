cd3806f43efc38b3aeb020d995b9a173ddcb9516
Niall Bennett <niallbennett@Nialls-MacBook-Pro.local>
2024-04-17 14:41:20 +0100

    portfolio button event test

--- tests/Portfolio.test.tsx
+++ tests/Portfolio.test.tsx
@@ -1,5 +1,6 @@
 import Portfolio from "../src/pagecomponents/Portfolio";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { it, expect, describe } from "vitest";
 import "@testing-library/jest-dom/vitest";
 import React from "react";
@@ -9,27 +10,38 @@ describe("portfolio", () => {
   it("should render with correct text and initial state", () => {
     render(
       <MemoryRouter>
-        <Portfolio
-          portfolio={[]}
-          addPortfolio={function (coin: object): void {
-            throw new Error("Function not implemented.");
-          }}
-          coins={[]}
-        />
+        <Portfolio portfolio={[]} addPortfolio={() => {}} coins={[]} />
       </MemoryRouter>
     );
     // find header
     const heading = screen.getByRole("heading", { level: 4 });
     expect(heading).toBeInTheDocument();
+  });
+});
 
+describe("portfolio buttons", () => {
+  const user = userEvent.setup(); // create user instance
+  it("should let user add coin and delete portfolio", async () => {
+    render(
+      // () => {} - mocks the func
+      <MemoryRouter>
+        <Portfolio portfolio={[]} addPortfolio={() => {}} coins={[]} />
+      </MemoryRouter>
+    );
     // find edit btn
     const editButton = screen.getByRole("button", { name: /add coin/i });
+    // test user click
+    await user.click(editButton);
+    // assert
     expect(editButton).toBeInTheDocument();
 
-    // delete button
+    // delete button - function needs to
     const deleteButton = screen.getByRole("button", {
       name: /delete portfolio/i,
     });
+    await user.click(deleteButton);
     expect(deleteButton).toBeInTheDocument();
   });
 });
+
+// test modals
