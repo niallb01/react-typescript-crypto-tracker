cce67209a09e224adfa20b19263d2b92b98004a7
Niall Bennett <niallbennett@Nialls-MacBook-Pro.local>
2024-04-19 11:58:53 +0100

    portfolio link

--- tests/Home.test.tsx
+++ tests/Home.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { it, expect, describe, test } from "vitest";
+import { it, expect, describe } from "vitest";
 import Home from "../src/pagecomponents/Home";
 import React from "react";
 import { MemoryRouter } from "react-router";
@@ -8,34 +8,45 @@ import userEvent from "@testing-library/user-event";
 
 describe("home", () => {
   it("should render with correct text and initial state", () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <Home coins={[]} portfolio={[]} addPortfolio={() => {}} />
-      </MemoryRouter>
-    );
-    // test input/portfolio link
-    const searchInput = screen.getByPlaceholderText(/search currency/i);
-    expect(searchInput).toBeInTheDocument();
-    // find portfolio link and test route
-    const portfolioLink = getByText(/portfolio/i);
-    expect(portfolioLink).toBeInTheDocument();
-    expect(portfolioLink.getAttribute("href")).toBe("/portfolio");
-  });
-});
-
-// test add coin to portfolio btn and toast
-describe("portfolio button", () => {
-  const user = userEvent.setup();
-  it("should add or remove portfolio coin", async () => {
     render(
       <MemoryRouter>
         <Home coins={[]} portfolio={[]} addPortfolio={() => {}} />
       </MemoryRouter>
     );
-    const addCoin = document.querySelector(
+    // test input
+    const searchInput = screen.getByPlaceholderText(/search currency/i);
+    expect(searchInput).toBeInTheDocument();
+  });
+});
+
+describe("portfolio link", () => {
+  const user = userEvent.setup();
+  it("should render portfolio page when navigating to portfolio page", async () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <Home coins={[]} portfolio={[]} addPortfolio={() => {}} />
+      </MemoryRouter>
+    );
+    const portfolioLink = getByText(/portfolio/i);
+    await user.click(portfolioLink);
+    expect(portfolioLink).toBeInTheDocument();
+    expect(portfolioLink.getAttribute("href")).toBe("/portfolio");
+  });
+});
+
+// test adding coin to portfolio btn and toast
+describe("portfolio button", () => {
+  const user = userEvent.setup();
+  it("should add or remove coin from portfolio", async () => {
+    render(
+      <MemoryRouter>
+        <Home coins={[]} portfolio={[]} addPortfolio={() => {}} />
+      </MemoryRouter>
+    );
+    const addCoinButton = document.querySelector(
       "svg"
     ) as unknown as HTMLImageElement;
-    await user.click(addCoin);
-    expect(addCoin).toBeInTheDocument();
+    await user.click(addCoinButton);
+    expect(addCoinButton).toBeInTheDocument();
   });
 });
