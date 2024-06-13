14c48acdeb5d2db8931e167a8c180f366c68e524
Niall Bennett <niallbennett@Nialls-MacBook-Pro.local>
2024-06-13 15:17:26 +0100

    change password

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -8,7 +8,6 @@ import { view } from "react-icons-kit/ikons/view";
 import { AccountProps } from "../types/auth_types";
 import "react-toastify/dist/ReactToastify.css";
 import { toast } from "react-toastify";
-// import { signOut } from "../auth/supabaseClient";
 
 const Account = (props: AccountProps) => {
   const [newPassword, setNewPassword] = useState("");
@@ -22,70 +21,65 @@ const Account = (props: AccountProps) => {
     addPortfolio,
   } = props;
 
-  const handleChangePassword = async () => {
+  const handleChangePassword = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
     const { error } = await supabase.auth.updateUser({
       password: newPassword,
     });
 
     if (error) {
-      return;
+      toast.error("Error changing password", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 2000,
+      });
     } else {
       toast.success("Password successfully changed", {
         position: toast.POSITION.TOP_CENTER,
         autoClose: 1000,
       });
+      setNewPassword("");
       setAuthenticated(false);
-      addPortfolio([]);
       navigate("/login");
+      addPortfolio([]);
     }
   };
-
-  // const handlePasswordLogout = async () => {
-  //   const success = await signOut();
-  //   if (success) {
-  //     setAuthenticated(false);
-  //     navigate("/login");
-  //     addPortfolio([]);
-  //   }
-  // };
 
   const onPasswordInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNewPassword(e.target.value);
   };
 
   return (
-    <>
-      <div className="account-form-container">
-        <h1 className="account-header">Change Password</h1>
-        <form>
-          <label className="password-label" htmlFor="password">
-            New Password:{" "}
-            <button
-              type="button"
-              onClick={onTogglePasswordVisibility}
-              className="password-icon-button"
-            >
-              <Icon icon={isPasswordVisible ? view : view_off} size="20" />
-            </button>
-          </label>{" "}
-          <input
-            type={isPasswordVisible ? "text" : "password"}
-            id="password"
-            name="password"
-            className="form-text-input"
-            value={newPassword}
-            onChange={onPasswordInput}
-            placeholder="New Test Password"
-            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}"
-            title="Must contain at least one number, one uppercase letter and special character, and at least 8 or more characters"
-            required
-          />
-          <button onClick={handleChangePassword} className="login-btn">
-            Confirm
+    <div className="account-form-container">
+      <h1 className="account-header">Change Password</h1>
+      <form onSubmit={handleChangePassword}>
+        <label className="password-label" htmlFor="password">
+          New Password:{" "}
+          <button
+            type="button"
+            onClick={onTogglePasswordVisibility}
+            className="password-icon-button"
+          >
+            <Icon icon={isPasswordVisible ? view : view_off} size="20" />
           </button>
-        </form>
-      </div>
-    </>
+        </label>{" "}
+        <input
+          type={isPasswordVisible ? "text" : "password"}
+          id="password"
+          name="password"
+          className="form-text-input"
+          value={newPassword}
+          onChange={onPasswordInput}
+          placeholder="New Test Password"
+          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}"
+          title="Must contain at least one number, one uppercase letter and special character, and at least 8 or more characters"
+          required
+        />
+        <button type="submit" className="login-btn">
+          Confirm
+        </button>
+      </form>
+    </div>
   );
 };
 
